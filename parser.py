310a0e8efd76b3c9cc46d1a4b11903bef344f216
jaityn <98439313+jaityn@users.noreply.github.com>
2022-02-21 16:15:22 -0600

    Update parser.py
    This is not complete yet, but a good start

--- parser.py
+++ parser.py
@@ -1,1 +1,33 @@
 
+## this is an attempt at some of the code that we talked about in class. It will more than likely have to be cleaned up to work, but it should be a good start for the project 
+
+
+import re
+from datetime import datetime
+
+from urllib.request import urlretrieve
+
+url='https://s3.amazonaws.com/tcmg476/http_access_log'
+local='localcopy.log'
+
+local, headers=urlretrieve(url, local)
+file=open(local,'r')
+
+
+# How many requests were made on a week-by-week basis? Per month?
+
+months = {
+  Jan: 0,
+  Feb: 0,
+  Mar: 0,
+  Apr: 0,
+  May: 0,
+  Jun: 0,
+  Jul: 0,
+  Aug: 0,
+  Sep: 0,
+  Oct: 0,
+  Nov: 0,
+  Dec: 0
+}
+
