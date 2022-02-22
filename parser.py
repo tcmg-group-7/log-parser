1f25cec5ba79dbc06d9049e0c63c211e528e8c34
shehzerp <99155873+shehzerp@users.noreply.github.com>
2022-02-22 14:00:43 -0600

    Update parser.py

--- parser.py
+++ parser.py
@@ -73,3 +73,18 @@ print(sum(month_values))
 # print out the dictionary totals
 print(days)
 print(months)
+
+#looks through the log file 
+for line in filelines:
+    if "[" in line:
+        totalrequests += 1
+        if re.search("\".*\" 4..", line) is not None:
+            error += 1
+        
+        # 3xx codes = redirected requests
+        if re.search("\".*\" 3..", line) is not None:
+           redirect += 1
+           
+print("Total number of requests:", totalrequests, "\n")
+print("Percentage of Unsuccessful Requests: ", round((error * 100) / totalrequests, 2), "%")
+print("Percentage of Requests Redirected: ", round((redirect * 100) / totalrequests, 2), "%")
