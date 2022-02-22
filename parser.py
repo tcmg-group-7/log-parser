59c55d59ab71ea8e80a4b33f26cb1c0927032bf7
shehzerp <99155873+shehzerp@users.noreply.github.com>
2022-02-22 13:14:58 -0600

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,1 +1,7 @@
+from urllib.request import urlretrieve
 
+url='https://s3.amazonaws.com/tcmg476/http_access_log'
+local='localcopy.log'
+
+local, headers=urlretrieve(url, local)
+file=open(local,'r')
