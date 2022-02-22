e1c29b52c9954fa60c2e51225ecf0ddc3a686ba7
mattzenger <mattzenger@gmail.com>
2022-02-22 13:25:21 -0600

    I added the requests for each weekday as well

--- parser.py
+++ parser.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import re
 
+# this is the requests that were made each month
 # opening the file and using the local log
 url = 'https://s3.amazonaws.com/tcmg476/http_access_log'
 local = 'http_access_log.txt'
@@ -9,6 +10,19 @@ file = openfile.read()
 filelines = file.split('\n')
 
 print('Loading...')
+
+# days dictionary
+
+days = {
+    'Sun': 0,
+    'Mon': 0,
+    'Tue': 0,
+    'Wed': 0,
+    'Thu': 0,
+    'Fri': 0,
+    'Sat': 0,
+    'Sun': 0
+}
 
 # months dictionary
 
@@ -27,6 +41,7 @@ months = {
     'December': 0
 }
 
+# for loop to find the number of requests for each month
 for line in filelines:
 
 # regex pattern to use
@@ -35,14 +50,26 @@ for line in filelines:
 # parts
 
     parts = regex.split(line)
+    # print(len(day_parts))
     # print(parts)
     if len(parts) != 5:
         continue
 
 # convert date string to a date object
     datestamp = datetime.strptime(parts[2], '%d/%b/%Y')
-    datestamp_conversion = datestamp.strftime('%B')
-    # print(datestamp)
-    months[datestamp_conversion] += 1
+# converting the date object to the date abbreviation
+    day_conversion = datestamp.strftime('%a')
+# converting the date object to the month full name
+    month_conversion = datestamp.strftime('%B')
+# when the conversions match the dictionary values, add to the counters
+    days[day_conversion] += 1
+    months[month_conversion] += 1
 
+day_values = days.values()
+month_values = months.values()
+print(sum(day_values))
+print(sum(month_values))
+
+# print out the dictionary totals
+print(days)
 print(months)
