027f202bb485aaa7363d3ceacf304c0a6c82fa5c
mattzenger <mattzenger@gmail.com>
2022-02-23 09:50:50 -0600

    I merged the least and most requested file into the main script

--- parser.py
+++ parser.py
@@ -143,4 +143,44 @@ redirectpercent= round((redirect * 100) / totalrequests, 2)
 
 print("Total number of requests:", totalrequests)
 print("Percentage of Unsuccessful Requests: ",str(errorpercent), "%")
-print("Percentage of Requests Redirected: ",str(redirectpercent), "%")
+print("Percentage of Requests Redirected: ",str(redirectpercent), "%\n")
+
+def find_most_least_requested_file():
+    log_line= 'local - - [24/Oct/1994:13:41:41 -0600] "GET index.html HTTP/1.0" 200 150'
+    pattern= r'\[([^:]*):(.*) .*\] \"([A-Z]*) (.*) HTTP.*\" ([2345]\d\d)'
+    collection_dict={}
+    # searches for matching pattern 
+    for line in filelines:
+        match= re.search(pattern,line)
+        try:
+            filename = match.groups()[3]
+        except AttributeError:
+            filename = match
+        if filename in collection_dict:
+            collection_dict[filename] += 1
+        else:
+            collection_dict[filename] = 1
+
+    # if the pattern is found, it is added to the dictionary 
+    values = list(collection_dict.values())
+    keys = list(collection_dict.keys())
+
+    # calculates the max and the minimum number of files 
+    m = max (values)
+    n = min (values)
+
+    # finding the values 
+    i = values.index(m)
+    z = values.index(n)
+
+    # gets the count of the keys which is the total number of times a file is requested
+    most_requested_file= keys[i]
+    least_requested_file= keys[z]
+
+    return "Most Requested",most_requested_file, "Least Requested",least_requested_file
+
+
+if __name__ == "__main__":
+    file_request= find_most_least_requested_file()
+    print(file_request) 
+print()
