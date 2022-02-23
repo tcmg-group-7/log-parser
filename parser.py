a3915ccc766e36e228081995712cefa94ee11cf3
jaityn <98439313+jaityn@users.noreply.github.com>
2022-02-22 23:35:53 -0600

    Update parser.py

--- parser.py
+++ parser.py
@@ -16,18 +16,51 @@ file=open(local,'r')
 
 # How many requests were made on a week-by-week basis? Per month?
 
-months = {
-  Jan: 0,
-  Feb: 0,
-  Mar: 0,
-  Apr: 0,
-  May: 0,
-  Jun: 0,
-  Jul: 0,
-  Aug: 0,
-  Sep: 0,
-  Oct: 0,
-  Nov: 0,
-  Dec: 0
+# months = {
+#  Jan: 0,
+#  Feb: 0,
+#  Mar: 0,
+#  Apr: 0,
+#  May: 0,
+#  Jun: 0,
+#  Jul: 0,
+#  Aug: 0,
+#  Sep: 0,
+#  Oct: 0,
+#  Nov: 0,
+#  Dec: 0
+#}
+
+
+awk 'BEGIN {
+    split("Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec ", months, " ")
+    for (a = 1; a <= 12; a++)
+        m[months[a]] = a
 }
+{
+    split($4,array,"[:/]");
+    year = array[3]
+    month = sprintf("%02d", m[array[2]])
+
+    print > FILENAME"-"year"_"month".txt"
+}' incendiary.ws-2009
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
